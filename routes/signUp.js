4f638fc2abb00d777203ff92a17211bd71205eb3
Alex Knott <alextknott@gmail.com>
2022-09-13 00:34:51 +0100

    Use controller function for /sign-up get request

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { sign_up_get } = require("../controllers/userController");
 
 /* GET home page. */
-router.get("/sign-up", function (req, res, next) {
-  res.render("signUp", { title: "Sign Up" });
-});
+router.get("/sign-up", sign_up_get);
 
 module.exports = router;
