e694f8569d734e0f4b9af15d8504ee4beff10950
Alex Knott <alextknott@gmail.com>
2022-09-12 23:17:10 +0100

    Connect app to mongoDB

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -12,6 +13,12 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+const { connect, connection } = require("mongoose");
+const mongoDB = process.env.mongoConnectionURL;
+connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
 app.use(logger("dev"));
 app.use(express.json());
