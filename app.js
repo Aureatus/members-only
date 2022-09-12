c2de8cd1be6bb92f9f41a1662521035108a3fe04
Alex Knott <alextknott@gmail.com>
2022-09-13 00:08:59 +0100

    Use signUp route with / prefix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var signUpRouter = require("./routes/signUp");
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/", signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
