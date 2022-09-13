3e75ef652a7d955befb818dd0ae81ce16c48235b
Alex Knott <alextknott@gmail.com>
2022-09-13 14:47:50 +0100

    Change password_name key to password

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const UserSchema = new Schema({
   user_name: { type: String, required: true },
   given_name: { type: String, required: true },
   family_name: { type: String, required: true },
-  password_name: { type: String, required: true },
+  password: { type: String, required: true },
   membership_status: { type: Boolean, required: true },
 });
 
