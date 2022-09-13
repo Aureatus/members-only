547aa50ef6781d3a8913eb379e472399e1f9492e
Alex Knott <alextknott@gmail.com>
2022-09-13 17:28:45 +0100

    Implement sign-in for users using passportjs

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var session = require("express-session");
+var bcrypt = require("bcryptjs");
+var User = require("./models/user");
 
 var indexRouter = require("./routes/index");
 var signUpRouter = require("./routes/signUp");
@@ -15,11 +20,47 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy(function verify(username, password, done) {
+    User.findOne({ user_name: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) {
+          return done(err);
+        }
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    });
+  })
+);
+
 const { connect, connection } = require("mongoose");
 const mongoDB = process.env.mongoConnectionURL;
 connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+app.use(passport.session());
 
 app.use(logger("dev"));
 app.use(express.json());
