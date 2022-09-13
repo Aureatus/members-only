206206ec5d81b1614b7303a134cf1b622993130c
Alex Knott <alextknott@gmail.com>
2022-09-13 14:39:02 +0100

    Add route for /sign-up post request

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var router = express.Router();
-const { sign_up_get } = require("../controllers/userController");
+const { sign_up_get, sign_up_post } = require("../controllers/userController");
 
 /* GET home page. */
 router.get("/sign-up", sign_up_get);
+
+router.post("/sign-up", sign_up_post);
 
 module.exports = router;
