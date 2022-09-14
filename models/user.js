9b93bdbded28c6ef3aee44d14b47c8bcfadb871b
Alex Knott <alextknott@gmail.com>
2022-09-14 15:54:41 +0100

    Add optional admin field

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   family_name: { type: String, required: true },
   password: { type: String, required: true },
   membership_status: { type: Boolean, required: true },
+  admin: { type: Boolean, required: true },
 });
 
 module.exports = model("User", UserSchema);
